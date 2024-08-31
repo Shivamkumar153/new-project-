e2f8508406c60d7079b18a008f62047deb17a98a
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2024-08-31 06:42:15 +0000

    variables in js

---
+++ 01_variable.js
@@ -0,0 +1,11 @@
+const accountId = "1234"
+let accountEmail = "shivamkumarr.1221@gmail.com" 
+var accountPassword ="1234"
+accountCity = "patna"
+
+accountEmail ="shivam@123gmail.com"
+accountPassword="3456"
+//accountId = "345"
+
+console.log("accountId");
+console.table([accountEmail,accountPassword,accountId,]);
