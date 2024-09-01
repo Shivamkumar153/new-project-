690f81b8c0bc4937a760908fb9e4fe58753017d1
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2024-09-01 12:49:41 +0000

    learning js

---
+++ basic/datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version .
+
+//alert("hello") //we are using node js ,not brower
+
+let name ="shivam";
+let age = 19;
+let isLoggedIn = false 
+/*
+number => 2 to power 53
+bigint
+string =>true/false
+null => standalon value
+undefined => 
+symbol=> unique
+
+
+object 
+*/
+console.log(typeof undefined);//undefine
+console.log(typeof null);//object
--- basic/variable.js
+++ basic/variable.js
@@ -1,11 +1,11 @@
-const accountId = "1234"
+const accountId = 1234
 let accountEmail = "shivamkumarr.1221@gmail.com" 
-var accountPassword ="1234"
+var accountPassword = 1234
 accountCity = "patna"
 
 accountEmail ="shivam@123gmail.com"
 accountPassword="3456"
-//accountId = "345"
+//accountId = "345" // not allowed 
 
 console.log("accountId");
-console.table([accountEmail,accountPassword,accountId,]);
+console.table([accountEmail,accountPassword,accountId,accountCity]);
