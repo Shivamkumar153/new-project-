bd3e7b46e8250a983d40458bbe0e95f4df5e2d07
Shivamkumar153 <shivamkumarr.1221@gmail.com>
2024-09-07 10:24:40 +0000

    conversion of data type

--- basic/datatype_conversion.js
+++ basic/datatype_conversion.js
@@ -1,5 +1,9 @@
-let score = 34;
+//"use strict";//treat all js code ba
+let score = "34"
 console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof score);
+console.log(typeof valueInNumber);
+
+let marks = 89.4
+console.log(typeof marks)
